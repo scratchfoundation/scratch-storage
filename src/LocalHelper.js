f56ebbcdbd91aa6abd71a989033ba785b1c26ab2
Christopher Willis-Ford <cwillisf@media.mit.edu>
2016-12-07 11:37:00 -0800

    Implement LocalHelper in terms of localForage

--- src/LocalHelper.js
+++ src/LocalHelper.js
@@ -1,23 +1,21 @@
+const localforage = require('localforage');
+
+const Asset = require('./Asset');
 const Helper = require('./Helper');
 
 /**
  * Implements storage on the local device, available even when the device has no network connection.
- * TODO: use one or more of the following APIs for the web, and files under `app.getPath('userData')` for Node.
- *       Maybe there's an existing Node module that would do that for us...?
- * Web storage APIs include:
- * - Local file system API:
- *     https://dev.w3.org/2009/dap/file-system/file-dir-sys.html
- *     https://developer.mozilla.org/en-US/docs/Web/API/LocalFileSystem
- * - IndexedDB:
- *     https://www.w3.org/TR/IndexedDB/
- *     https://developer.mozilla.org/en-US/docs/Web/API/IndexedDB_API
- * - Web SQL Database:
- *     https://www.w3.org/TR/webdatabase/
- * - Local Web Storage:
- *     https://www.w3.org/TR/webstorage/
- *     https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API
  */
 class LocalHelper extends Helper {
+
+    constructor (parent) {
+        super(parent);
+
+        localforage.config({
+            name: 'Scratch 3.0',
+            size: 100 * 1024 * 1024
+        });
+    }
 
     /**
      * Fetch an asset but don't process dependencies.
@@ -25,8 +23,17 @@ class LocalHelper extends Helper {
      * @param {string} assetId - The ID of the asset to fetch: a project ID, MD5, etc.
      * @return {Promise.<Asset>} A promise for the contents of the asset.
      */
-    load (assetType, assetId) { // eslint-disable-line no-unused-vars
-        return Promise.reject(new Error('LocalHelper not yet implemented'));
+    load (assetType, assetId) {
+        return new Promise((fulfill, reject) => {
+            const fileName = [assetId, assetType.runtimeFormat].join('.');
+            localforage.getItem(fileName).then(
+                data => {
+                    fulfill(new Asset(assetType, assetId, assetType.runtimeFormat, data));
+                },
+                error => {
+                    reject(error);
+                });
+        });
     }
 }
 
