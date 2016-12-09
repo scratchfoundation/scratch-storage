1ed665bb571386c0fea12964f841540f4a6809a1
Christopher Willis-Ford <cwillisf@media.mit.edu>
2016-12-09 14:27:22 -0800

    Set `src/.eslintrc.js` to root as well
    The code in `src/` is intended for use in both Node and on the web, but
    will be processed by Webpack which emulates a subset of Node's features
    when targeting the web. This change separates the linting rules for
    `src/` from the root of the project (which is intended for Node
    directly) and enforces Node-like linting rules without actually
    including Node globals. If we end up needing any of the Node globals
    provided by Webpack, we can add them explicitly here.

--- src/.eslintrc.js
+++ src/.eslintrc.js
@@ -1,7 +1,8 @@
 module.exports = {
+    root: true,
+    extends: ['scratch', 'scratch/es6', 'scratch/node'],
     env: {
         node: false,
         browser: false
-    },
-    extends: ['scratch/es6']
+    }
 };
