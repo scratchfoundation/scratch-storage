28598754ef1eaed7d15876efd91b1ea84d9bd3ca
Christopher Willis-Ford <cwillisf@media.mit.edu>
2017-03-22 18:54:36 -0700

    Host tests in a jsdom environment
    This allows us to test code meant for a browser even though the tests
    are running in Node.

--- test/unit/load-default-assets.js
+++ test/unit/load-default-assets.js
@@ -1,51 +1,65 @@
 const crypto = require('crypto');
 const test = require('tap').test;
 
-const ScratchStorage = require('../../dist/node/scratch-storage');
-const {Asset, AssetType} = ScratchStorage;
+const jsdomTest = require('../fixtures/jsdomTest');
 
-const defaultAssetTypes = [AssetType.ImageBitmap, AssetType.ImageVector, AssetType.Sound];
-const defaultIds = {};
+/**
+ * These tests will run inside the jsdom "window" after loading the script under test.
+ * @param {object} window - the jsdom window.
+ */
+const hostedTests = function (window) {
+    const ScratchStorage = window.Scratch.Storage;
+    const {Asset, AssetType} = ScratchStorage;
 
-let storage;
-test('constructor', t => {
-    storage = new ScratchStorage();
-    t.type(storage, ScratchStorage);
-    t.end();
-});
+    const defaultAssetTypes = [AssetType.ImageBitmap, AssetType.ImageVector, AssetType.Sound];
+    const defaultIds = {};
 
-test('getDefaultAssetId', t => {
-    for (let i = 0; i < defaultAssetTypes.length; ++i) {
-        const assetType = defaultAssetTypes[i];
-        const id = storage.getDefaultAssetId(assetType);
-        t.type(id, 'string');
-        defaultIds[assetType.name] = id;
-    }
-    t.end();
-});
+    let storage;
+    test('constructor', t => {
+        storage = new ScratchStorage();
+        t.type(storage, ScratchStorage);
+        t.end();
+    });
 
-test('load', t => {
-    const promises = [];
-    for (let i = 0; i < defaultAssetTypes.length; ++i) {
-        const assetType = defaultAssetTypes[i];
-        const id = defaultIds[assetType.name];
+    test('getDefaultAssetId', t => {
+        for (let i = 0; i < defaultAssetTypes.length; ++i) {
+            const assetType = defaultAssetTypes[i];
+            const id = storage.getDefaultAssetId(assetType);
+            t.type(id, 'string');
+            defaultIds[assetType.name] = id;
+        }
+        t.end();
+    });
 
-        const promise = storage.load(assetType, id);
-        t.type(promise, 'Promise');
+    test('load', t => {
+        const promises = [];
+        for (let i = 0; i < defaultAssetTypes.length; ++i) {
+            const assetType = defaultAssetTypes[i];
+            const id = defaultIds[assetType.name];
 
-        promises.push(promise);
+            const promise = storage.load(assetType, id);
+            t.type(promise, 'Promise');
 
-        promise.then(asset => {
-            t.type(asset, Asset);
-            t.strictEqual(asset.assetId, id);
-            t.strictEqual(asset.assetType, assetType);
-            t.ok(asset.data.length);
+            promises.push(promise);
 
-            const hash = crypto.createHash('md5');
-            hash.update(asset.data);
-            t.strictEqual(hash.digest('hex'), id);
-        });
-    }
+            promise.then(asset => {
+                t.type(asset, Asset);
+                t.strictEqual(asset.assetId, id);
+                t.strictEqual(asset.assetType, assetType);
+                t.ok(asset.data.length);
 
-    return Promise.all(promises);
-});
+                const hash = crypto.createHash('md5');
+                hash.update(asset.data);
+                t.strictEqual(hash.digest('hex'), id);
+            });
+        }
+
+        return Promise.all(promises);
+    });
+};
+
+jsdomTest(
+    hostedTests,
+    '<html><body></body></html>',
+    [require.resolve('../../dist/web/scratch-storage')]
+);
