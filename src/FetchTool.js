29542fa0b76eef4f18232f0afd09cd5d67db2824
Paul Kaplan <pkaplan@media.mit.edu>
2019-08-22 10:57:38 -0400

    Return status code in case of failed POST
    Fixes https://github.com/LLK/scratch-storage/issues/100

--- src/FetchTool.js
+++ src/FetchTool.js
@@ -42,7 +42,10 @@ class FetchTool {
         return fetch(url, Object.assign({
             credentials: withCredentials ? 'include' : 'omit'
         }, options))
-            .then(result => result.text());
+            .then(result => {
+                if (!result.ok) throw new Error(result.status);
+                return result.text());
+            });
     }
 }
 
