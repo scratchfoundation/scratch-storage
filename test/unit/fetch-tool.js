0e52c4a2a4265cdfd2d433daed368c89d5f31ec2
Andrey Nikitin <anikitin@codio.com>
2021-11-29 11:18:28 +0300

    fix(fetchtool): catch fetch exception on get resource

--- test/unit/fetch-tool.js
+++ test/unit/fetch-tool.js
@@ -1,4 +1,6 @@
 const test = require('tap').test;
+const TextEncoder = require('util').TextEncoder;
+const TextDecoder = require('util').TextDecoder;
 
 const FetchTool = require('../../src/FetchTool');
 
@@ -24,6 +26,37 @@ test('send failure returns response.status', t => {
     const tool = new FetchTool();
 
     return tool.send('url').catch(reason => {
+        t.equal(reason, 500);
+    });
+});
+
+test('get success returns Uint8Array.body(response.arrayBuffer())', t => {
+    const text = 'successful response';
+    const encoding = 'utf-8';
+    const encoded = new TextEncoder().encode(text);
+    const decoder = new TextDecoder(encoding);
+
+    global.fetch = () => Promise.resolve({
+        ok: true,
+        arrayBuffer: () => encoded.buffer
+    });
+
+    const tool = new FetchTool();
+    
+    return tool.get({url: 'url'}).then(result => {
+        t.equal(decoder.decode(result), text);
+    });
+});
+
+test('get failure returns response.status', t => {
+    global.fetch = () => Promise.resolve({
+        ok: false,
+        status: 500
+    });
+
+    const tool = new FetchTool();
+
+    return tool.get({url: 'url'}).catch(reason => {
         t.equal(reason, 500);
     });
 });
