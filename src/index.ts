da54f3616f963ee0874b26c6d3c4530de24f0bf5
Georgi Angelov <gangelov@asteasolutions.com>
2024-10-10 21:58:32 +0300

    chore: export some types

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
 import {ScratchStorage} from './ScratchStorage';
+import Asset, {AssetId} from './Asset';
+import {AssetType} from './AssetType';
+import {DataFormat} from './DataFormat';
+import Helper from './Helper';
 
 /**
  * Export for use with NPM & Node.js.
  * @type {ScratchStorage}
  */
 export default ScratchStorage;
+
+export {
+  Asset,
+  AssetId,
+  AssetType,
+  DataFormat,
+  Helper
+};
